d06cf62f0bc6a62c637a0ac069535fe34cdc9782
Pedro Lorandi <hello@pedrolorandi.com>
2022-11-16 14:10:30 -0500

    Added message after connection and display name of snake

--- client.js
+++ client.js
@@ -10,6 +10,11 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write("Name: PKL");
+  });
+
   conn.on('data', (data) => {
     console.log(data);
   });
